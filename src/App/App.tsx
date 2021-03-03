7ba37c8357a7e27ecb1bae7ca059a7dbfce777b0
psina <psiniy-programmer@yandex.ru>
2021-03-03 21:06:14 +0300

    new webpack

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 function App() {
     return (
+        <Profile />
         <div className={'App'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, saepe.</div>
     );
 }
