b0b640767ad2784e1284c30189b2d6e5ad91926f
grind-t <grindt.gd@gmail.com>
2021-03-07 22:00:49 +0300

    refactor control components to shared styles

--- src/App/AuthPage/SignIn/SignIn.tsx
+++ src/App/AuthPage/SignIn/SignIn.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Input from '@components/form/controls/Input';
-import Button from '@components/form/controls/Button';
 import styles from './SignIn.module.scss';
 
 function SignIn() {
@@ -8,11 +6,11 @@ function SignIn() {
         <React.Fragment>
             <h2 className={styles.heading}>Авторизация</h2>
             <form>
-                <Input aria-label="Логин" placeholder="Логин:" className={styles.login} />
-                <Input aria-label="Пароль" placeholder="Пароль:" className={styles.password} />
+                <input aria-label="Логин" placeholder="Логин:" className={styles.login} />
+                <input aria-label="Пароль" placeholder="Пароль:" className={styles.password} />
                 <span className={styles.forgotPassword}>Забыли пароль?</span>
-                <Button className={styles.primaryButton}>Войти</Button>
-                <Button className={styles.secondaryButton}>Создать аккаунт</Button>
+                <button className={styles.signInButton}>Войти</button>
+                <button className={styles.signUpButton}>Создать аккаунт</button>
             </form>
         </React.Fragment>
     );
