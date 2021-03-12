6312aebba56f252198ad6d6e033e0774bf115b61
grind-t <grindt.gd@gmail.com>
2021-03-12 23:26:25 +0300

    Create AccountType for SignUp

---
+++ src/App/SignUpPage/AccountType/AccountType.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, useRouteMatch, useHistory } from 'react-router-dom';
+import headingIcon from './heading-icon.png';
+import goBackIcon from './go-back-icon.svg';
+import styles from './AccountType.module.scss';
+
+function AccountType() {
+    const { url } = useRouteMatch();
+    const history = useHistory();
+
+    return (
+        <React.Fragment>
+            <img src={headingIcon} alt="Иконка" className={styles.headingIcon} />
+            <h1 className={styles.heading}>Создать аккаунт как:</h1>
+            <Link to={`${url}/parent`} className={styles.link}>
+                <span className={styles.linkText}>Родитель</span>
+            </Link>
+            <Link to={`${url}/child`} className={styles.link}>
+                <span className={styles.linkText}>Специалист</span>
+            </Link>
+            <Link to={`${url}/specialist`} className={styles.link}>
+                <span className={styles.linkText}>Ребенок</span>
+            </Link>
+            <button onClick={() => history.goBack()} className={styles.goBack}>
+                <img src={goBackIcon} alt="Иконка" className={styles.goBackIcon}/>
+                <span>Вернуться назад</span>
+            </button>
+        </React.Fragment>
+    );
+}
+
+export default AccountType;
--- src/App/SignUpPage/SignUpPage.tsx
+++ src/App/SignUpPage/SignUpPage.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import FormPageTemplate from '@components/form/FormPageTemplate';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import AccountType from './AccountType';
 
 function SignUpPage() {
+    const { path } = useRouteMatch();
+
     return (
         <FormPageTemplate>
+            <Switch>
+                <Route exact path={path}>
+                    <AccountType />
+                </Route>
+                <Route path={`${path}/parent`}>
 
+                </Route>
+                <Route path={`${path}/child`}>
+
+                </Route>
+                <Route path={`${path}/specialist`}>
+
+                </Route>
+            </Switch>
         </FormPageTemplate>
     );
 }
