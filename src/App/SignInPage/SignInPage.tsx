1ef4603a9dbe9112a08e7dc1aba0678db2a58b06
grind-t <grindt.gd@gmail.com>
2021-03-14 15:47:32 +0300

    refactor SignInPage

--- src/App/SignInPage/SignInPage.tsx
+++ src/App/SignInPage/SignInPage.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import FormPageTemplate from '@components/form/FormPageTemplate';
+import React, { useState } from 'react';
+import FormPageTemplate from '@components/templates/FormPageTemplate';
 import styles from './SignInPage.module.scss';
+import { Link } from 'react-router-dom';
 
 function SignInPage() {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <FormPageTemplate>
             <h1 className={styles.heading}>Авторизация</h1>
             <form>
-                <input aria-label="Логин" placeholder="Логин:" className={styles.login} />
-                <input aria-label="Пароль" placeholder="Пароль:" className={styles.password} />
-                <span className={styles.forgotPassword}>Забыли пароль?</span>
-                <button className={styles.signInButton}>Войти</button>
-                <button className={styles.signUpButton}>Создать аккаунт</button>
+                <input 
+                    type="email"
+                    aria-label="Логин" 
+                    placeholder="Логин:" 
+                    value={login}
+                    onChange={(e) => setLogin(e.target.value)}
+                    className={styles.login} 
+                />
+                <input
+                    type="password"
+                    aria-label="Пароль" 
+                    placeholder="Пароль:" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className={styles.password} 
+                />
+                <Link to="/TODO" className={styles.forgotPassword}>
+                    Забыли пароль?
+                </Link>
+                <button className={styles.signInButton}>
+                    Войти
+                </button>
+                <Link to="/signup" className={styles.signUpButton}>
+                    Создать аккаунт
+                </Link>
             </form>
         </FormPageTemplate>
     );
