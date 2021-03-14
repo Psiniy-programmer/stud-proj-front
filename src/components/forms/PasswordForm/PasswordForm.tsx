f0fd0219da2e72129add9e81cf7811e8123cc78e
grind-t <grindt.gd@gmail.com>
2021-03-14 23:35:46 +0300

    create PasswordForm

---
+++ src/components/forms/PasswordForm/PasswordForm.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import styles from './PasswordForm.module.scss';
+
+export interface Password {
+    password: string;
+}
+
+interface PasswordFormProps {
+    submitText: string;
+    onSubmit: (password: Password) => any;
+}
+
+function PasswordForm({ submitText, onSubmit }: PasswordFormProps) {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    return (
+        <form onSubmit={() => onSubmit({password})}>
+            <input 
+                type="password"
+                name="password"
+                aria-label="Пароль"
+                placeholder="Пароль"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className={styles.field}
+            />
+            <input 
+                type="password"
+                aria-label="Повтор пароля"
+                placeholder="Повтор пароля"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className={styles.field}
+            />
+            <button className={styles.submit}>
+                {submitText}
+            </button>
+        </form>
+    );
+}
+
+export default PasswordForm;
